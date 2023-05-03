4eb17aeb3886fc746ca8d31cc91ccf38830ad703
BenediktSos <sosnowskybene@gmail.com>
2023-05-03 19:25:31 +0200

    Improved gesture recognition for extended thumb

--- HandRecognition/hand.py
+++ HandRecognition/hand.py
@@ -34,7 +34,13 @@ class Hand:
         if finger is HandRegion.THUMB:
             thumb_vec = [self.landmark[4].x - self.landmark[2].x,
                          self.landmark[4].y - self.landmark[2].y]
-            return self.landmark[2].x > self.landmark[4].x
+            intersection_pnt = get_intersect([self.landmark[4].x, self.landmark[4].y],
+                                             [self.landmark[2].x, self.landmark[2].y],
+                                             [self.landmark[0].x, self.landmark[0].y],
+                                             [self.landmark[9].x, self.landmark[9].y])
+            helper_vec = [intersection_pnt[0] - self.landmark[0].x,
+                          intersection_pnt[1] - self.landmark[0].y]
+            return dot(hand_vec, helper_vec) < 0
         elif finger is HandRegion.INDEX:
             index_vec = [self.landmark[8].x - self.landmark[6].x,
                          self.landmark[8].y - self.landmark[6].y]
@@ -42,21 +48,21 @@ class Hand:
         elif finger is HandRegion.MIDDLE:
             middle_vec = [self.landmark[12].x - self.landmark[10].x,
                           self.landmark[12].y - self.landmark[10].y]
-            return self.landmark[10].y > self.landmark[12].y
+            return dot(hand_vec, middle_vec) > 0
         elif finger is HandRegion.RING:
             ring_vec = [self.landmark[16].x - self.landmark[14].x,
                         self.landmark[16].y - self.landmark[14].y]
-            return self.landmark[14].y > self.landmark[16].y
+            return dot(hand_vec, ring_vec) > 0
         elif finger is HandRegion.PINKY:
             pinky_vec = [self.landmark[20].x - self.landmark[18].x,
                          self.landmark[20].y - self.landmark[18].y]
-            return self.landmark[18].y > self.landmark[20].y
+            return dot(hand_vec, pinky_vec) > 0
         else:
             return False
 
     def draw_vecs(self, image, dims):
         draw_arrow = lambda vec, pnt: cv2.arrowedLine(image, [int(c) for c in pnt], [int(c) for c in pnt + vec],
-                                                     (0, 0, 255), 2) if vec.all() and pnt.all() else None
+                                                      (0, 0, 255), 2) if vec.all() and pnt.all() else None
 
         hand_vec = [self.landmark[9].x - self.landmark[0].x,
                     self.landmark[9].y - self.landmark[0].y] * dims
@@ -92,6 +98,33 @@ class Hand:
 
 def dot(vec1, vec2):
     return np.vdot(vec1, vec2)
+
+
+def angle_between(v1, v2):
+    """
+    Returns the angle in radians between vectors 'v1' and 'v2'::
+    """
+    v1 = v1 / np.linalg.norm(v1)
+    v2 = v2 / np.linalg.norm(v2)
+    return np.arccos(np.clip(np.dot(v1, v2), -1.0, 1.0))
+
+
+def get_intersect(a1, a2, b1, b2):
+    """
+    Returns the point of intersection of the lines passing through a2,a1 and b2,b1.
+    a1: [x, y] a point on the first line
+    a2: [x, y] another point on the first line
+    b1: [x, y] a point on the second line
+    b2: [x, y] another point on the second line
+    """
+    s = np.vstack([a1, a2, b1, b2])  # s for stacked
+    h = np.hstack((s, np.ones((4, 1))))  # h for homogeneous
+    l1 = np.cross(h[0], h[1])  # get first line
+    l2 = np.cross(h[2], h[3])  # get second line
+    x, y, z = np.cross(l1, l2)  # point of intersection
+    if z == 0:  # lines are parallel
+        return (float('inf'), float('inf'))
+    return (x / z, y / z)
 
 
 class HandRegion(Enum):
