df38f0d134a299d601ac7ef4b12667dcc9d4b791
Haraldr <fridrich1900@gmail.com>
2023-05-20 15:35:43 +0200

    modelmuster

---
+++ model/extract_and_save_landmarks.py
@@ -0,0 +1,97 @@
+import mediapipe as mp
+import numpy as np
+import os
+import cv2
+
+"""
+    For each gesture it's own directory:
+    left click
+    right click
+    scroll up
+    scroll down
+    scroll wheel click
+    hold on
+    default
+"""
+
+mp_hands = mp.solutions.hands
+
+IMG_DIR = os.path.join("..", "daten")
+LEFT_CLICK_DIR = os.path.join(IMG_DIR, "left_click")
+RIGHT_CLICK_DIR = os.path.join(IMG_DIR, "right_click")
+SCROLL_UP_DIR = os.path.join(IMG_DIR, "scroll_up")
+SCROLL_DOWN_DIR = os.path.join(IMG_DIR, "scroll_down")
+SCROLL_WHEEL_CLICK_DIR = os.path.join(IMG_DIR, "scroll_wheel_click")
+HOLD_ON_DIR = os.path.join(IMG_DIR, "hold_on")
+DEFAULT_DIR = os.path.join(IMG_DIR, "default")
+
+left_click_files = [os.path.join(LEFT_CLICK_DIR, name) for name in os.listdir(LEFT_CLICK_DIR)]
+right_click_files = [os.path.join(RIGHT_CLICK_DIR, name) for name in os.listdir(RIGHT_CLICK_DIR)]
+scroll_up_files = [os.path.join(SCROLL_UP_DIR, name) for name in os.listdir(SCROLL_UP_DIR)]
+scroll_down_files = [os.path.join(SCROLL_DOWN_DIR, name) for name in os.listdir(SCROLL_DOWN_DIR)]
+scroll_wheel_click_files = [os.path.join(SCROLL_WHEEL_CLICK_DIR, name) for name in os.listdir(SCROLL_WHEEL_CLICK_DIR)]
+hold_on_files = [os.path.join(HOLD_ON_DIR, name) for name in os.listdir(HOLD_ON_DIR)]
+default_files = [os.path.join(DEFAULT_DIR, name) for name in os.listdir(DEFAULT_DIR)]
+
+all_files = {
+    "left_click": left_click_files,
+    "right_click": right_click_files,
+    "scroll_up": scroll_up_files,
+    "scroll_down": scroll_down_files,
+    "scroll_wheel_click": scroll_wheel_click_files,
+    "hold_on": hold_on_files,
+    "default": default_files
+}
+
+X = []
+y = []
+
+with mp_hands.Hands(
+        model_complexity=1,
+        min_detection_confidence=0.8,
+        min_tracking_confidence=0.8,
+        max_num_hands=2) as hands:
+
+    for label, files in all_files.items():
+
+
+        for file in files:
+            image = cv2.imread(file, cv2.IMREAD_COLOR)
+            image = cv2.flip(image, 1)
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
+
+            if results.multi_hand_landmarks:
+                for index, class_i in enumerate(results.multi_handedness):
+                    hand_label = class_i.classification[0].label
+                    hand_landmarks = results.multi_hand_landmarks[index].landmark
+
+                    landmarks = []
+                    for landmark in hand_landmarks:
+                        landmarks.append([landmark.x, landmark.y])
+
+                    X.append(landmarks)
+
+                    label_code = [0]*7
+                    if label == "left_click":
+                        label_code[0] = 1
+                    elif label == "right_click":
+                        label_code[1] = 1
+                    elif label == "scroll_up":
+                        label_code[2] = 1
+                    elif label == "scroll_down":
+                        label_code[3] = 1
+                    elif label == "scroll_wheel_click":
+                        label_code[4] = 1
+                    elif label == "hold_on":
+                        label_code[5] = 1
+                    else:
+                        label_code[6] = 1
+                    y.append(label_code)
+
+X = np.array(X)
+y = np.array(y)
+
+np.save(os.path.join(IMG_DIR, "X.npy"), X)
+np.save(os.path.join(IMG_DIR, "y.npy"), y)
---
+++ model/model_train.py
@@ -0,0 +1,86 @@
+import keras as k
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+X = np.load(os.path.join("..", "daten", "X.npy"))
+y = np.load(os.path.join("..", "daten", "y.npy"))
+
+X = np.reshape(X, (X.shape[0], 21, 2, 1))
+y = np.reshape(y, (y.shape[0], 7))
+
+
+def make_model(input_shape):
+    input_layer = k.layers.Input(input_shape)
+
+    conv = k.layers.Conv2D(filters=64, kernel_size=3, padding="same")(input_layer)
+    conv = k.layers.BatchNormalization()(conv)
+    conv = k.layers.MaxPooling2D(pool_size=3, padding="same")(conv)
+
+    conv = k.layers.Conv2D(filters=32, kernel_size=3, padding="same")(conv)
+    conv = k.layers.BatchNormalization()(conv)
+    conv = k.layers.ReLU()(conv)
+
+    conv = k.layers.Conv2D(filters=16, kernel_size=3, padding="same")(conv)
+    conv = k.layers.BatchNormalization()(conv)
+    conv = k.layers.ReLU()(conv)
+
+    conv = k.layers.Conv2D(filters=8, kernel_size=2, padding="same")(conv)
+    conv = k.layers.BatchNormalization()(conv)
+    conv = k.layers.ReLU()(conv)
+
+    gap = k.layers.GlobalAvgPool2D()(conv)
+    gap = k.layers.Dense(100)(gap)
+
+    output_layer = k.layers.Dense(7, activation="softmax")(gap)
+
+    return k.models.Model(inputs=input_layer, outputs=output_layer)
+
+model = make_model(X.shape[1:])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True)
+model.compile(
+    optimizer="sgd",
+    loss=k.losses.CategoricalCrossentropy(),
+    metrics=["categorical_accuracy", "Recall"]
+)
+
+callbacks = [
+    k.callbacks.ModelCheckpoint(
+        "best_model.h5", save_best_only=True, monitor="val_loss"
+    ),
+    k.callbacks.ReduceLROnPlateau(
+        monitor="val_loss", factor=0.5, patience=50, verbose=1
+    ),
+    k.callbacks.EarlyStopping(
+        monitor="val_loss", patience=50, verbose=1
+    )
+]
+
+history = model.fit(
+    X_train,
+    y_train,
+    batch_size=32,
+    epochs=5,
+    validation_split=0.15,
+    verbose=1,
+    callbacks=callbacks
+)
+
+plt.figure()
+plt.plot(history.history["loss"])
+plt.plot(history.history["val_loss"])
+plt.title("Loss")
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.savefig("loss.png")
+
+plt.figure()
+plt.plot(history.history["categorical_accuracy"])
+plt.plot(history.history["val_categorical_accuracy"])
+plt.title("categorical_accuracy")
+plt.xlabel("epoch")
+plt.ylabel("accuracy")
+plt.savefig("categorical_accuracy.png")
